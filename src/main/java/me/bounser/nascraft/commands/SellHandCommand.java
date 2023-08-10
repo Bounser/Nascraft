fa3c23b33e5c28dcc2959d74b640014a85e76425
Bounser <jtorralbomartin@gmail.com>
2023-08-10 13:30:41 +0200

    New command: "/sell" - opens a (chest) GUI to sell items.

---
+++ src/main/java/me/bounser/nascraft/commands/SellHandCommand.java
@@ -0,0 +1,80 @@
+package me.bounser.nascraft.commands;
+
+import me.bounser.nascraft.Nascraft;
+import me.bounser.nascraft.market.managers.MarketManager;
+import me.bounser.nascraft.config.Config;
+import me.bounser.nascraft.market.RoundUtils;
+import net.md_5.bungee.api.chat.ClickEvent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
+import net.md_5.bungee.api.chat.HoverEvent;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import me.bounser.nascraft.market.unit.Item;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+
+public class SellHandCommand implements CommandExecutor {
+
+    private HashMap<Player, ItemStack> players = new HashMap();
+
+    private Config lang = Config.getInstance();
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+
+        if (!(sender instanceof Player)) {
+            Nascraft.getInstance().getLogger().info(ChatColor.RED  + "Command not available through console.");
+        }
+
+        Player player = ((Player) sender).getPlayer();
+
+        if (!player.hasPermission("nascraft.sellhand")) {
+            player.sendMessage(lang.getPermissionText());
+            return false;
+        }
+
+        assert player != null;
+        ItemStack handItems = player.getInventory().getItemInMainHand();
+
+        if (args.length == 0) {
+
+            Item item = MarketManager.getInstance().getItem(handItems.getType().toString());
+
+            if(item != null) {
+
+                String text = ChatColor.GRAY + "Estimated value: \n\n> " + ChatColor.GOLD + item.getName() +" x "+ handItems.getAmount() +" = "+ RoundUtils.round(handItems.getAmount()*item.getPrice().getSellPrice()) + Config.getInstance().getCurrency() + "\n  ";
+
+                TextComponent message = new TextComponent(Config.getInstance().getClickToConfirmText());
+                message.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/nsellhand confirm"));
+                message.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new ComponentBuilder(text).create()));
+
+                player.spigot().sendMessage(message);
+                players.put(player, handItems);
+
+            } else {
+                player.sendMessage(ChatColor.RED + "Hold a valid item!");
+            }
+
+        } else if (args[0].equalsIgnoreCase("confirm")) {
+
+            if(player.getInventory().getItemInMainHand().equals(players.get(player))) {
+                Item item = MarketManager.getInstance().getItem(player.getInventory().getItemInMainHand().getType().toString());
+
+                item.sellItem(handItems.getAmount(), player, 1);
+            } else {
+                Bukkit.broadcastMessage(ChatColor.RED + "Error. Don't change the items in your hand when confirming");
+            }
+        }
+
+
+        return false;
+    }
+
+}
---
+++ src/main/java/me/bounser/nascraft/commands/sellinv/SellInvCommand.java
@@ -0,0 +1,82 @@
+package me.bounser.nascraft.commands.sellinv;
+
+import me.bounser.nascraft.Nascraft;
+import me.bounser.nascraft.config.Config;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+
+public class SellInvCommand implements CommandExecutor {
+
+    private Config lang = Config.getInstance();
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+
+
+        if(!(sender instanceof Player)) {
+            Nascraft.getInstance().getLogger().info(ChatColor.RED  + "Command not available through console.");
+            return false;
+        }
+
+        Player player = (Player) sender;
+
+        if (!player.hasPermission("nascraft.sellinv")) {
+            player.sendMessage(lang.getPermissionText());
+            return false;
+        }
+
+        Inventory inventory = Bukkit.createInventory(player, 45, lang.getSellTitle());
+
+        insertFillingPanes(inventory);
+        insertSellButton(inventory);
+        insertCloseButton(inventory);
+
+        player.openInventory(inventory);
+        return false;
+    }
+
+    public void insertFillingPanes(Inventory inventory) {
+
+        ItemStack filler = new ItemStack(Material.BLACK_STAINED_GLASS_PANE);
+        ItemMeta meta = filler.getItemMeta();
+        meta.setDisplayName(" ");
+        filler.setItemMeta(meta);
+
+        for(int i : new int[]{0, 1, 2, 3, 4, 5, 6, 7, 36, 37, 38, 39, 41, 42, 43, 44}) {
+            inventory.setItem(i, filler);
+        }
+    }
+
+    public void insertSellButton(Inventory inventory) {
+
+        ItemStack sellButton = new ItemStack(Material.YELLOW_STAINED_GLASS_PANE);
+        ItemMeta meta = sellButton.getItemMeta();
+        meta.setDisplayName(lang.getSellButtonName());
+        meta.setLore(Collections.singletonList(lang.getSellButtonLore("0")));
+        sellButton.setItemMeta(meta);
+
+        inventory.setItem(40, sellButton);
+    }
+
+    public void insertCloseButton(Inventory inventory) {
+
+        ItemStack closeButton = new ItemStack(Material.RED_STAINED_GLASS_PANE);
+        ItemMeta meta = closeButton.getItemMeta();
+        meta.setDisplayName(lang.getSellCloseText());
+        closeButton.setItemMeta(meta);
+
+        inventory.setItem(8, closeButton);
+    }
+
+}
