1257bdd5d5d785eba8740b0cd5356ab623952d21
Bounser <jtorralbomartin@gmail.com>
2023-09-21 22:04:16 +0200

    New pricing system.

--- src/main/java/me/bounser/nascraft/market/unit/Price.java
+++ src/main/java/me/bounser/nascraft/market/unit/Price.java
@@ -1,7 +1,8 @@
 package me.bounser.nascraft.market.unit;
 
 import me.bounser.nascraft.config.Config;
-import me.bounser.nascraft.market.RoundUtils;
+import me.bounser.nascraft.formatter.RoundUtils;
+import org.bukkit.Bukkit;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,97 +12,73 @@ public class Price {
     // Current neutral price
     private float value;
 
-    // The counter will keep track of the short-term changes on stock.
-    private int counter = 0;
+    private float initialValue;
 
-    // Tolerance threshold. Once the stock changes this amount the price will be affected.
-    private final int threshold;
-
-    private int stock;
+    private float stock;
 
     // Advanced metrics
     private final float support;
     private final float resistance;
     private final float noiseIntensity;
 
+    private final float elasticity;
+
     private float historicalHigh;
+    private float historicalLow;
 
     private float hourHigh;
     private float hourLow;
     private final List<Float> dayHigh = new ArrayList<>();
     private final List<Float> dayLow = new ArrayList<>();
 
-    public Price(float price, int stock, float historicalHigh, float elasticity, float support, float resistance, float noiseIntensity) {
-        value = price;
+    public Price(float initialValue, int stock, float historicalHigh, float elasticity, float support, float resistance, float noiseIntensity) {
+
+        value = (float) (initialValue * Math.exp(-0.0005 * elasticity * stock));
+
+        this.initialValue = initialValue;
 
         this.stock = stock;
         this.historicalHigh = historicalHigh;
-        hourHigh = price;
-        hourLow = price;
+        hourHigh = value;
+        hourLow = value;
         dayHigh.add(hourHigh);
         dayLow.add(hourLow);
         this.support = support;
         this.resistance = resistance;
         this.noiseIntensity = noiseIntensity;
-
-        threshold = Math.max(Math.round(64*(1/elasticity)), 1);
+        this.elasticity = elasticity;
     }
 
     public float getValue() { return RoundUtils.round(value); }
 
     public float getBuyPrice() {
 
-        if (value * Config.getInstance().getTaxBuy()-value < 0.01) {
-            return RoundUtils.round((float) (value + 0.01));
+        if (value * Config.getInstance().getTaxBuy() < 0.01) {
+            return (float) (value + 0.01);
         } else {
-            return RoundUtils.round(value * Config.getInstance().getTaxBuy());
+            return value * Config.getInstance().getTaxBuy();
         }
     }
 
     public float getSellPrice() {
 
         if (value - value*Config.getInstance().getTaxSell() < 0.01) {
-            return RoundUtils.round((float) (value + 0.01));
+            return (float) (value - 0.01);
         } else {
-            return RoundUtils.round(value * Config.getInstance().getTaxSell());
+            return value * Config.getInstance().getTaxSell();
         }
     }
 
     public void setStock(int stock) { this.stock = stock; }
 
-    public int getStock() { return stock; }
+    public float getStock() { return stock; }
 
     public void changeStock(int change) {
+        stock += change;
 
-        counter += change;
-
-        while(counter <= -threshold) {
-
-            if(stock <= 0)
-                value += Math.max(RoundUtils.round((float) (value * 0.01 * Math.log10(-stock+10))), 0.01);
-            else
-                value += Math.max(RoundUtils.round((float) (value * 0.01)), 0.01);
-
-            verifyChange();
-
-            counter += threshold;
-        }
-
-        while(counter >= threshold) {
-
-            if(stock > 0)
-                value -= Math.max(RoundUtils.round((float) (value * 0.01 * Math.log10(stock+10))), 0.01);
-            else
-                value -= Math.max(RoundUtils.round((float) (value * 0.01)), 0.01);
-
-            verifyChange();
-
-            counter -= threshold;
-        }
+        value = (float) (initialValue * Math.exp(-0.0005 * elasticity * stock));
 
         updateLimits();
-
-        stock += change;
     }
 
     public void verifyChange() {
@@ -113,17 +90,19 @@ public class Price {
 
         if (support != 0 && value < support && Math.random() > 0.8) {
 
-            value = RoundUtils.preciseRound((float) (value * (0.99 + 0.03 * Math.random() * noiseIntensity)));
+            stock -= (8 - 12 * Math.random()) * noiseIntensity;
 
         } else if (resistance != 0 && value > resistance && Math.random() > 0.8) {
 
-            value = RoundUtils.preciseRound((float) (value * (1.01 - 0.03 * Math.random() * noiseIntensity)));
+            stock += (8 - 12 * Math.random()) * noiseIntensity;
 
         } else {
 
-            value = RoundUtils.preciseRound((float) (value * ((1 - 0.01 * noiseIntensity + 0.02 * Math.random() * noiseIntensity))));
+            stock += (10 - 20 * Math.random()) * noiseIntensity;
 
         }
+
+        value = (float) (initialValue * Math.exp(-0.0005 * elasticity * stock));
         verifyChange();
 
         updateLimits();
@@ -131,11 +110,17 @@ public class Price {
 
     public float getHistoricalHigh() { return historicalHigh; }
 
+    public float getHistoricalLow() { return historicalLow; }
+
+    public void setHistoricalHigh(float newHistoricalHigh) { historicalHigh = newHistoricalHigh; }
+
+    public void setHistoricalLow(float newHistoricalLow) { historicalLow = newHistoricalLow; }
+
     public float getDayHigh() {
 
         float high = dayHigh.get(0);
 
-        for (float value : dayHigh) if(value > high) high = value;
+        for (float value : dayHigh) if (value > high) high = value;
 
         return Math.max(hourHigh, high);
 
@@ -145,21 +130,22 @@ public class Price {
 
         float low = dayLow.get(0);
 
-        for(float value : dayLow) if(low > value) low = value;
+        for (float value : dayLow) if (low > value) low = value;
 
-        return Math.max(hourLow, low);
+        return Math.min(hourLow, low);
     }
 
     public void updateLimits() {
-        if(value > historicalHigh) { historicalHigh = value; }
+        if (value > historicalHigh) { historicalHigh = value; }
+        if (value < historicalLow) { historicalLow = value; }
 
-        if(value > hourHigh) { hourHigh = value; }
-        if(value < hourLow) { hourLow = value; }
+        if (value > hourHigh) { hourHigh = value; }
+        if (value < hourLow) { hourLow = value; }
     }
 
     public void restartHourLimits() {
 
-        if(dayHigh.size() == 24) {
+        if (dayHigh.size() == 24) {
             dayHigh.remove(0);
             dayLow.remove(0);
 
