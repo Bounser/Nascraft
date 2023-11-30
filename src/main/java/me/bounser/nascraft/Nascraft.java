0431864a25ab9c476589bdaa0024ba4fd714b92e
Bounser <jtorralbomartin@gmail.com>
2023-11-30 20:01:52 +0100

    Process of layout (Nascraft.json) injection changed.

--- src/main/java/me/bounser/nascraft/Nascraft.java
+++ src/main/java/me/bounser/nascraft/Nascraft.java
@@ -1,21 +1,29 @@
 package me.bounser.nascraft;
 
-import de.leonhard.storage.Json;
-import de.leonhard.storage.util.FileUtils;
 import me.bounser.nascraft.advancedgui.LayoutModifier;
-import me.bounser.nascraft.commands.MarketCommand;
-import me.bounser.nascraft.commands.NascraftCommand;
-import me.bounser.nascraft.commands.SellHandCommand;
+import me.bounser.nascraft.commands.*;
+import me.bounser.nascraft.commands.alert.SetAlertCommand;
+import me.bounser.nascraft.commands.alert.AlertsCommand;
+import me.bounser.nascraft.commands.discord.DiscordCommand;
+import me.bounser.nascraft.commands.discord.DiscordInventoryInGame;
 import me.bounser.nascraft.commands.sellall.SellAllCommand;
 import me.bounser.nascraft.commands.sellall.SellAllTabCompleter;
-import me.bounser.nascraft.commands.sellinv.InventoryListener;
+import me.bounser.nascraft.commands.sellinv.SellInvListener;
 import me.bounser.nascraft.commands.sellinv.SellInvCommand;
-import me.bounser.nascraft.database.Data;
+import me.bounser.nascraft.database.SQLite;
+import me.bounser.nascraft.discord.DiscordBot;
+import me.bounser.nascraft.discord.linking.LinkCommand;
+import me.bounser.nascraft.market.managers.BrokersManager;
 import me.bounser.nascraft.market.managers.MarketManager;
 import me.bounser.nascraft.placeholderapi.PAPIExpansion;
 import me.bounser.nascraft.config.Config;
+import me.leoko.advancedgui.AdvancedGUI;
+import me.leoko.advancedgui.manager.GuiItemManager;
+import me.leoko.advancedgui.manager.GuiWallManager;
 import me.leoko.advancedgui.manager.LayoutManager;
 import me.bounser.nascraft.bstats.Metrics;
+import me.leoko.advancedgui.utils.VersionMediator;
+import org.apache.commons.io.FileUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -25,13 +33,13 @@ import net.milkbowl.vault.economy.Economy;
 import java.io.*;
 import java.util.List;
 
+
 public final class Nascraft extends JavaPlugin {
 
     private static Nascraft main;
     private static Economy economy = null;
 
-    // AdvancedGUI version used to test the plugin.
-    private final String AdvancedGUI_version = "2.2.6";
+    private static final String AdvancedGUI_version = "2.2.7";
 
     public static Nascraft getInstance() { return main; }
 
@@ -42,7 +50,7 @@ public final class Nascraft extends JavaPlugin {
 
         new Metrics(this, 18404);
 
-        Config.getInstance();
+        Config config = Config.getInstance();
 
         if (!setupEconomy()) {
             getLogger().severe("Nascraft failed to load! Vault is required.");
@@ -51,37 +59,65 @@ public final class Nascraft extends JavaPlugin {
         }
 
         if (Bukkit.getPluginManager().getPlugin("AdvancedGUI") == null) {
-            getLogger().warning("AdvancedGUI is not installed! Graphs won't work without it!");
+            getLogger().warning("AdvancedGUI is not installed! You won't have graphs in-game without it!");
             getLogger().warning("Learn more about AdvancedGUI here: https://www.spigotmc.org/resources/83636/");
         } else if (!Bukkit.getPluginManager().getPlugin("AdvancedGUI").getDescription().getVersion().equals(AdvancedGUI_version)){
             getLogger().warning("This plugin was made using AdvancedGUI " + AdvancedGUI_version + "! You may encounter errors on other versions");
         }
 
         if(Bukkit.getPluginManager().getPlugin("PlaceholderAPI") != null) {
-            new PAPIExpansion(this).register();
+            getLogger().info("PlaceholderAPI detected!");
+            new PAPIExpansion().register();
         }
 
-        if (Config.getInstance().getCheckResources()) { checkResources(); }
+        if(config.getDiscordEnabled()) {
+            getLogger().info("Enabling discord integration!");
+
+            getCommand("link").setExecutor(new LinkCommand());
+            getCommand("setalert").setExecutor(new SetAlertCommand());
+            getCommand("alerts").setExecutor(new AlertsCommand());
+            getCommand("discord").setExecutor(new DiscordCommand());
+
+            Bukkit.getPluginManager().registerEvents(new DiscordInventoryInGame(), this);
+
+            new DiscordBot();
+        }
+
+        if (config.getCheckResources()) checkResources();
 
         MarketManager.getInstance();
+        BrokersManager.getInstance();
+
+        LayoutManager.getInstance().registerLayoutExtension(LayoutModifier.getInstance(), this);
 
         getCommand("nascraft").setExecutor(new NascraftCommand());
         getCommand("market").setExecutor(new MarketCommand());
 
-        List<String> commands = Config.getInstance().getCommands();
+        List<String> commands = config.getCommands();
+        if(commands == null) return;
+
         if(commands.contains("sellhand")) getCommand("sellhand").setExecutor(new SellHandCommand());
-        if(commands.contains("sellall")) getCommand("sellall").setExecutor(new SellAllCommand());
-        if(commands.contains("sell")) getCommand("sellinv").setExecutor(new SellInvCommand());
+        if(commands.contains("sell")) {
+            getCommand("sellinv").setExecutor(new SellInvCommand());
+            Bukkit.getPluginManager().registerEvents(new SellInvListener(), this);
+        }
+        if(commands.contains("sellall")) {
+            getCommand("sellall").setExecutor(new SellAllCommand());
+            getCommand("sellall").setTabCompleter(new SellAllTabCompleter());
+        }
 
-        getCommand("sellall").setTabCompleter(new SellAllTabCompleter());
-
-        Bukkit.getPluginManager().registerEvents(new InventoryListener(), this);
-
-        LayoutManager.getInstance().registerLayoutExtension(LayoutModifier.getInstance(), this);
     }
 
     @Override
-    public void onDisable() { Data.getInstance().shutdownDatabase(); }
+    public void onDisable() {
+        SQLite.getInstance().shutdown();
+
+        if (Config.getInstance().getDiscordEnabled()) {
+            DiscordBot.getInstance().removeAllMessages();
+            DiscordBot.getInstance().sendClosedMessage();
+            DiscordBot.getInstance().getJDA().shutdown();
+        }
+    }
 
     public static Economy getEconomy() { return economy; }
 
@@ -100,32 +136,39 @@ public final class Nascraft extends JavaPlugin {
         getLogger().info("Checking required layouts... ");
         getLogger().info("If you want to disable this procedure, set auto_resources_injection to false in the config.yml file.");
 
-        File toLayout0 = new File(getDataFolder().getParent() + "/AdvancedGUI/layout/Nascraft.json");
+        File fileToReplace = new File(getDataFolder().getParent() + "/AdvancedGUI/layout/Nascraft.json");
 
-        if (!toLayout0.exists()) {
-            InputStream fromLayout0 = getResource("Nascraft.json");
-            assert fromLayout0 != null;
-            FileUtils.writeToFile(toLayout0, fromLayout0);
+        if (!fileToReplace.exists()) {
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(getResource("Nascraft.json")));
+                StringBuilder jsonContent = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    jsonContent.append(line);
+                }
+                reader.close();
+
+                FileUtils.writeStringToFile(fileToReplace, jsonContent.toString(), "UTF-8");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
             getLogger().info("Layout Nascraft.json added.");
 
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ag reload");
+            LayoutManager.getInstance().shutdownSync();
+            GuiWallManager.getInstance().shutdown();
+            GuiItemManager.getInstance().shutdown();
+
+            Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
+                AdvancedGUI.getInstance().readConfig();
+                VersionMediator.reload();
+                LayoutManager.getInstance().reload(layout -> getLogger().severe("§cFailed to load layout: " + layout + " §7(see console for details)"));
+                Bukkit.getScheduler().runTask(AdvancedGUI.getInstance(), () -> {
+                    GuiWallManager.getInstance().setup();
+                    GuiItemManager.getInstance().setup();
+                });
+            });
         } else {
-            Json layout = new Json("Nascraft", getDataFolder().getParent() + "/AdvancedGUI/layout/");
-
-            String ver = layout.getString("layoutVersion");
-
-            if(ver == null || !ver.equals(getDescription().getVersion())) {
-                getLogger().info("Layout outdated, updating...");
-
-                InputStream fromLayout0 = getResource("Nascraft.json");
-                assert fromLayout0 != null;
-                FileUtils.writeToFile(toLayout0, fromLayout0);
-                getLogger().info("Layout Nascraft.json updated. (From version " + ver + " to " + getDescription().getVersion() + ")" );
-
-                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ag reload");
-                return;
-            }
-            getLogger().info("Layout present and in the correct version.");
+            getLogger().info("Layout (Nascraft.json) present!");
         }
     }
 }
