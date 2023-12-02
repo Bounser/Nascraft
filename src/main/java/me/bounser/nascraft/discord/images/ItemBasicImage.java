3c2101eb0057e0caa891166e7df43b7ed69c2e3b
Bounser <jtorralbomartin@gmail.com>
2023-12-02 11:24:16 +0100

    New basic image of item.

--- src/main/java/me/bounser/nascraft/discord/images/ItemBasicImage.java
+++ src/main/java/me/bounser/nascraft/discord/images/ItemBasicImage.java
@@ -20,11 +20,11 @@ import java.util.Objects;
 
 public class ItemBasicImage {
 
-    private static Lang lang = Lang.get();
+    private static final Lang lang = Lang.get();
 
     public static BufferedImage getImage(Item item) {
 
-        BufferedImage image = new BufferedImage(8*128, 4*128, BufferedImage.TYPE_INT_ARGB);
+        BufferedImage image = new BufferedImage(8*128, 5*128, BufferedImage.TYPE_INT_ARGB);
 
         Graphics graphics = image.getGraphics();
 
@@ -53,62 +53,96 @@ public class ItemBasicImage {
             graphics2D.setColor(new Color(150, 30, 30));
         }
 
-        graphics2D.drawPolyline(item.getPlotData().getXPositions(backgroundImage.getWidth(), 0, false) , item.getPlotData().getYPositions(backgroundImage.getHeight(), 1, false), item.getPlotData().getNPoints(false));
-        graphics2D.drawPolyline(item.getPlotData().getXPositions(backgroundImage.getWidth(), 1, false) , item.getPlotData().getYPositions(backgroundImage.getHeight(), 2, false), item.getPlotData().getNPoints(false));
-        graphics2D.drawPolyline(item.getPlotData().getXPositions(backgroundImage.getWidth(), -1, false) , item.getPlotData().getYPositions(backgroundImage.getHeight(), 2, false), item.getPlotData().getNPoints(false));
+        graphics2D.drawPolyline(item.getPlotData().getXPositions(backgroundImage.getWidth(), 0, false) , item.getPlotData().getYPositions(backgroundImage.getHeight(), 1, false, false), item.getPlotData().getNPoints(false));
+        graphics2D.drawPolyline(item.getPlotData().getXPositions(backgroundImage.getWidth(), 1, false) , item.getPlotData().getYPositions(backgroundImage.getHeight(), 2, false, false), item.getPlotData().getNPoints(false));
+        graphics2D.drawPolyline(item.getPlotData().getXPositions(backgroundImage.getWidth(), -1, false) , item.getPlotData().getYPositions(backgroundImage.getHeight(), 2, false, false), item.getPlotData().getNPoints(false));
 
         graphics2D.setComposite(AlphaComposite.Clear);
 
         graphics2D.fillPolygon(
                 item.getPlotData().getXPositions(backgroundImage.getWidth(), 0, true),
-                item.getPlotData().getYPositions(backgroundImage.getHeight(), 0, true),
+                item.getPlotData().getYPositions(backgroundImage.getHeight(), 0, true, false),
                 item.getPlotData().getNPoints(true));
 
         graphics2D.setComposite(AlphaComposite.SrcOver);
 
         graphics2D.dispose();
 
-        graphics.drawImage(tempImage, 0, 50, 8*128, 175*2, null);
+        graphics.setColor(new Color(43,45,49));
+        graphics.fillRect(0, 450, 8*128, 100);
 
-        graphics.setColor(new Color(41, 43, 55));
+        graphics.drawImage(tempImage, 0, 128, 1024, 370, null);
 
-        graphics.fillRect(0, 200*2, 8*128, 56*2);
+        int[] height = item.getPlotData().getExtremePositions(0, backgroundImage.getHeight());
+        if (height[0] != 0 && height[1] != 0) {
 
-        graphics.drawImage(Images.getInstance().getImage(item.getMaterial(), 100, 100, false), 18, 0, 100, 100, null);
+            if(item.getPlotData().isGoingUp())
+                graphics.setColor(new Color(100, 250, 100));
+            else
+                graphics.setColor(new Color(250, 100, 100));
 
-        graphics.setColor(new Color(100, 255, 100));
-        graphics.setFont(new Font("Arial", Font.BOLD, 35));
+            graphics.setFont(new Font("Arial", Font.BOLD, 22));
 
-        graphics.drawString(Formatter.format(item.getPrice().getValue(), Style.ROUND_TO_TWO), 20, 440);
+            float[] high = item.getPlotData().getHighestValue(8*128, item.getPrices(TimeSpan.HOUR).size());
+            float[] low = item.getPlotData().getLowestValue(8*128, item.getPrices(TimeSpan.HOUR).size());
 
-        graphics.setFont(new Font("Arial", Font.BOLD, 22));
-        graphics.setColor(new Color(80, 155, 80));
+            drawCenteredString(graphics, Formatter.format(high[0], Style.ROUND_BASIC), Math.round(high[1]), 135, 8*128);
+            drawCenteredString(graphics, Formatter.format(low[0], Style.ROUND_BASIC), Math.round(low[1]), 475, 8*128);
+        }
 
-        graphics.drawString(lang.message(Message.DISCORD_BUY) + Formatter.format(item.getPrice().getBuyPrice(), Style.ROUND_TO_TWO), 25, 473);
+        graphics.setColor(new Color(69, 69, 79));
+        graphics.fillRoundRect(5, 0, 8*128-5, 100, 40, 40);
+        graphics.fillRoundRect(0, 195*2+128, 8*128, 61*2, 40, 40);
 
-        graphics.setColor(new Color(155, 80, 80));
+        graphics.setColor(new Color(99, 99, 119));
+        graphics.fillRoundRect(0, 0, 110, 100, 40, 40);
+        graphics.fillRoundRect(0, 195*2+128, 270, 61*2, 40, 40);
 
-        graphics.drawString(lang.message(Message.DISCORD_SELL) + Formatter.format(item.getPrice().getSellPrice(), Style.ROUND_TO_TWO), 25, 500);
+        graphics.drawImage(Images.getInstance().getImage(item.getMaterial(), 100, 100, false), 5, 0, 100, 100, null);
+
+        graphics.setFont(new Font("Arial", Font.BOLD, 26));
+        graphics.setColor(new Color(150, 255, 150));
+
+        graphics.drawString(lang.message(Message.DISCORD_BUY) + Formatter.format(item.getPrice().getBuyPrice(), Style.ROUND_BASIC), 20, 440+128);
+
+        graphics.setColor(new Color(255, 150, 150));
+
+        graphics.drawString(lang.message(Message.DISCORD_SELL) + Formatter.format(item.getPrice().getSellPrice(), Style.ROUND_BASIC), 20, 480+128);
 
 
-        graphics.setFont(new Font("Arial", Font.BOLD, 45));
+        graphics.setFont(new Font("Arial", Font.BOLD, 47));
 
         graphics.setColor(new Color(255, 255, 255));
-        graphics.drawString(item.getName(), 67*2, 33*2);
+        graphics.drawString(item.getName() + " | " + Formatter.format(item.getPrice().getValue(), Style.ROUND_BASIC), 67*2, 34*2);
 
-        graphics.setFont(new Font("Arial", Font.BOLD, 22));
+        graphics.setFont(new Font("Arial", Font.BOLD, 21));
 
-        graphics.drawString(lang.message(Message.DISCORD_DAY_HIGH) + Formatter.format(item.getPrice().getDayHigh(), Style.ROUND_TO_TWO), 250, 431);
-        graphics.drawString(lang.message(Message.DISCORD_DAY_LOW) + Formatter.format(item.getPrice().getDayLow(), Style.ROUND_TO_TWO), 250, 465);
-        graphics.drawString(lang.message(Message.DISCORD_HISTORICAL_HIGH) + Formatter.format(item.getPrice().getHistoricalHigh(), Style.ROUND_TO_TWO), 250, 499);
-        graphics.drawString(lang.message(Message.DISCORD_DAILY_VOLUME) + Formatter.format(item.getVolume(), Style.ROUND_TO_TWO), 650, 431);
-        graphics.drawString(lang.message(Message.DISCORD_POSITION) + MarketManager.getInstance().getPositionByVolume(item), 650, 465);
+        graphics.drawString(lang.message(Message.DISCORD_DAY_HIGH) + Formatter.format(item.getPrice().getDayHigh(), Style.ROUND_BASIC), 290, 420+128);
+        graphics.drawString(lang.message(Message.DISCORD_DAY_LOW) + Formatter.format(item.getPrice().getDayLow(), Style.ROUND_BASIC), 290, 460+128);
+        graphics.drawString(lang.message(Message.DISCORD_HISTORICAL_HIGH) + Formatter.format(item.getPrice().getHistoricalHigh(), Style.ROUND_BASIC), 290, 500+128);
+        graphics.drawString(lang.message(Message.DISCORD_DAILY_VOLUME) + Formatter.format(item.getVolume(), Style.REDUCED_LENGTH), 670, 420+128);
+        graphics.drawString(lang.message(Message.DISCORD_POSITION) + MarketManager.getInstance().getPositionByVolume(item), 670, 460+128);
         NumberFormat formatter = new DecimalFormat("#0.0");
-        graphics.drawString(lang.message(Message.DISCORD_TREND) + formatter.format((-100 + item.getPrice().getValue()*100/item.getPrices(TimeSpan.DAY).get(0))) + "%" , 650, 499);
+        graphics.drawString(lang.message(Message.DISCORD_TREND) + formatter.format((-100 + item.getPrice().getValue()*100/item.getPrices(TimeSpan.DAY).get(0))) + "%" , 670, 500+128);
 
         graphics.dispose();
 
         return image;
+    }
+
+    public static void drawCenteredString(Graphics g, String text, int centerX, int y, int canvasWidth) {
+        FontMetrics fm = g.getFontMetrics();
+        int textWidth = fm.stringWidth(text);
+
+        int x = centerX - (textWidth / 2);
+
+        if (x < 0) {
+            x = 0;
+        } else if (x + textWidth > canvasWidth) {
+            x = canvasWidth - textWidth;
+        }
+
+        g.drawString(text, x, y);
     }
 
 }
--- src/main/java/me/bounser/nascraft/market/unit/PlotData.java
+++ src/main/java/me/bounser/nascraft/market/unit/PlotData.java
@@ -1,5 +1,6 @@
 package me.bounser.nascraft.market.unit;
 
+import me.bounser.nascraft.formatter.RoundUtils;
 import me.bounser.nascraft.market.resources.TimeSpan;
 
 import java.text.DecimalFormat;
@@ -16,9 +17,13 @@ public class PlotData {
 
     public PlotData(Item item) { this.item = item; }
 
-    public int[] getYPositions(int size, int offset, boolean polygon) {
+    public int[] getYPositions(int size, int offset, boolean polygon, boolean precise) {
 
-        List<Float> values = item.getPrices(TimeSpan.HALFHOUR);
+        List<Float> values = item.getPrices(TimeSpan.HOUR);
+
+        if (!precise) {
+            values.replaceAll(RoundUtils::round);
+        }
 
         // Y points
         if(polygon) {
@@ -26,7 +31,6 @@ public class PlotData {
         } else {
             y = new int[values.size()];
         }
-
 
         float maxValue = Collections.max(values);
         float minValue = Collections.min(values);
@@ -48,9 +52,9 @@ public class PlotData {
 
     public int[] getXPositions(int size, int offset, boolean polygon) {
 
-        List<Float> values = item.getPrices(TimeSpan.HALFHOUR);
+        List<Float> values = item.getPrices(TimeSpan.HOUR);
 
-        int z = Math.round((float) size /(values.size()-1));
+        float z = (float) size /(values.size()-1);
 
         if(polygon) {
             x = new int[values.size()+2];
@@ -59,11 +63,9 @@ public class PlotData {
         }
 
         int j;
-        for (j = 0; j < (values.size()-1); j++) {
-            x[j] = (z*j + offset);
+        for (j = 0; j < (values.size()); j++) {
+            x[j] = Math.round(z*j + offset);
         }
-
-        x[j] = offset + size + 1;
 
         if(polygon) {
             x[values.size()] = size + offset;
@@ -73,21 +75,49 @@ public class PlotData {
         return x;
     }
 
-    public int getNPoints(boolean polygon) { return polygon ? item.getPrices(TimeSpan.HALFHOUR).size() + 2 : item.getPrices(TimeSpan.HALFHOUR).size(); }
+    public int getNPoints(boolean polygon) { return polygon ? item.getPrices(TimeSpan.HOUR).size() + 2 : item.getPrices(TimeSpan.HOUR).size(); }
 
     public boolean isGoingUp() {
 
-        List<Float> values = item.getPrices(TimeSpan.HALFHOUR);
+        List<Float> values = item.getPrices(TimeSpan.HOUR);
 
         return values.get(0) < values.get(values.size()-1);
     }
 
     public String getChange() {
 
-        List<Float> values = item.getPrices(TimeSpan.HALFHOUR);
+        List<Float> values = item.getPrices(TimeSpan.HOUR);
         NumberFormat formatter = new DecimalFormat("#0.0");
 
         return formatter.format((-100 + values.get(values.size()-1)*100/values.get(0))) + "%";
+    }
+
+    public float[] getLowestValue(int size, int points) {
+
+        float z = (float) size /(points-1);
+
+        float min = Collections.min(item.getPrices(TimeSpan.HOUR));
+
+        return new float[]{min, z*item.getPrices(TimeSpan.HOUR).lastIndexOf(min)};
+    }
+
+    public float[] getHighestValue(int size, int points) {
+
+        float z = (float) size /(points-1);
+
+        float max = Collections.max(item.getPrices(TimeSpan.HOUR));
+
+        return new float[]{max, z*item.getPrices(TimeSpan.HOUR).lastIndexOf(max)};
+    }
+
+    public int[] getExtremePositions(int offset, int size) {
+
+        if (Collections.min(item.getPrices(TimeSpan.HOUR)) == Collections.max(item.getPrices(TimeSpan.HOUR)))
+            return new int[]{0, 0};
+
+        int graphArea = (int) Math.round(size*0.9);
+        return new int[]{offset + size-graphArea, offset + graphArea};
+
     }
 
 }
