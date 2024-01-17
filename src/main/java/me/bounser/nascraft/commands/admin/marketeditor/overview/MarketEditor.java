eb6d3238568c50b733938c6e7a1431583ddb7d7f
Bounser <jtorralbomartin@gmail.com>
2024-01-17 08:11:52 +0100

    Horizontal offset in market editor.

--- src/main/java/me/bounser/nascraft/commands/admin/marketeditor/overview/MarketEditor.java
+++ src/main/java/me/bounser/nascraft/commands/admin/marketeditor/overview/MarketEditor.java
@@ -2,6 +2,7 @@ package me.bounser.nascraft.commands.admin.marketeditor.overview;
 
 import me.bounser.nascraft.config.lang.Lang;
 import me.bounser.nascraft.config.lang.Message;
+import me.bounser.nascraft.market.MarketManager;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
@@ -22,8 +23,10 @@ public class MarketEditor {
         insertFillingPanes(inventory);
         insertArrows(inventory);
         insertHelpHead(inventory);
-        insertHopper(inventory);
-        MarketEditorInvListener.getInstance().insertItems(inventory, 3);
+        insertButtons(inventory);
+        MarketEditorInvListener.getInstance().insertItems(inventory, 3, 0);
+
+        MarketEditorInvListener.getInstance().initializeOffsets(player, 3, 0);
 
         player.openInventory(inventory);
 
@@ -37,13 +40,13 @@ public class MarketEditor {
         metaBlack.setDisplayName(" ");
         blackFiller.setItemMeta(metaBlack);
 
-        for(int i : new int[]{1, 2, 3, 5, 6, 7, 46, 47, 48, 50, 51, 52, 53}) {
+        for(int i : new int[]{1, 2, 3, 5, 6, 7, 46, 47, 49, 51}) {
             inventory.setItem(i, blackFiller);
         }
 
         ItemStack closeButton = new ItemStack(Material.RED_STAINED_GLASS_PANE);
         ItemMeta meta = closeButton.getItemMeta();
-        meta.setDisplayName(Lang.get().message(Message.SELL_CLOSE));
+        meta.setDisplayName(ChatColor.RED + "§lCLOSE");
         closeButton.setItemMeta(meta);
 
         inventory.setItem(8, closeButton);
@@ -62,11 +65,21 @@ public class MarketEditor {
         arrow.setItemMeta(meta);
 
         inventory.setItem(45, arrow);
+
+        meta.setDisplayName(ChatColor.LIGHT_PURPLE + "§l< LEFT");
+        arrow.setItemMeta(meta);
+
+        inventory.setItem(52, arrow);
+
+        meta.setDisplayName(ChatColor.LIGHT_PURPLE + "§lRIGHT >");
+        arrow.setItemMeta(meta);
+
+        inventory.setItem(53, arrow);
     }
 
     public void insertHelpHead(Inventory inventory) {
 
-        ItemStack info = new ItemStack(Material.BOOK);
+        ItemStack info = new ItemStack(Material.CHEST);
         ItemMeta meta = info.getItemMeta();
         meta.setDisplayName(ChatColor.DARK_PURPLE + "§lMARKET EDITOR");
         meta.setLore(Arrays.asList(
@@ -78,18 +91,57 @@ public class MarketEditor {
         inventory.setItem(4, info);
     }
 
-    public void insertHopper(Inventory inventory) {
+    public void insertButtons(Inventory inventory) {
 
-        ItemStack info = new ItemStack(Material.HOPPER);
-        ItemMeta meta = info.getItemMeta();
-        meta.setDisplayName(ChatColor.GREEN + "§lADD ITEM TO MARKET");
-        meta.setLore(Arrays.asList(
+        ItemStack newItem = new ItemStack(Material.HOPPER);
+        ItemMeta metaNewItem = newItem.getItemMeta();
+        metaNewItem.setDisplayName(ChatColor.GREEN + "§lADD ITEM TO MARKET");
+        metaNewItem.setLore(Arrays.asList(
                 ChatColor.GRAY + "Drop here an item to configure",
                 ChatColor.GRAY + "it as a new item."
         ));
-        info.setItemMeta(meta);
+        newItem.setItemMeta(metaNewItem);
 
-        inventory.setItem(49, info);
+        inventory.setItem(50, newItem);
+
+        ItemStack enabled;
+        ItemMeta metaEnabled;
+
+        if (MarketManager.getInstance().getActive()) {
+            enabled = new ItemStack(Material.LIME_DYE);
+
+            metaEnabled = enabled.getItemMeta();
+            metaEnabled.setDisplayName(ChatColor.GREEN + "§lMARKET ACTIVE");
+            metaEnabled.setLore(Arrays.asList(
+                    ChatColor.GRAY + "Click to stop the market.",
+                    ChatColor.GRAY + "Users won't be able to buy/sell."
+            ));
+
+        } else {
+            enabled = new ItemStack(Material.RED_DYE);
+
+            metaEnabled = enabled.getItemMeta();
+            metaEnabled.setDisplayName(ChatColor.RED + "§lMARKET STOPPED");
+            metaEnabled.setLore(Arrays.asList(
+                    ChatColor.GRAY + "Click to resume the market.",
+                    ChatColor.GRAY + "Users will be able to buy/sell."
+            ));
+        }
+
+        enabled.setItemMeta(metaEnabled);
+
+        inventory.setItem(49, enabled);
+
+        ItemStack category = new ItemStack(Material.WRITABLE_BOOK);
+        ItemMeta metaCategory = category.getItemMeta();
+        metaCategory.setDisplayName(ChatColor.GREEN + "§lEDIT CATEGORIES");
+        metaCategory.setLore(Arrays.asList(
+                ChatColor.GRAY + "Click here to edit and add",
+                ChatColor.GRAY + "new categories."
+        ));
+        category.setItemMeta(metaCategory);
+
+        inventory.setItem(48, category);
     }
 
 
--- src/main/java/me/bounser/nascraft/commands/admin/marketeditor/overview/MarketEditorInvListener.java
+++ src/main/java/me/bounser/nascraft/commands/admin/marketeditor/overview/MarketEditorInvListener.java
@@ -1,6 +1,7 @@
 package me.bounser.nascraft.commands.admin.marketeditor.overview;
 
 import me.bounser.nascraft.commands.admin.marketeditor.edit.EditorManager;
+import me.bounser.nascraft.commands.admin.marketeditor.edit.category.CategoryEditor;
 import me.bounser.nascraft.formatter.Formatter;
 import me.bounser.nascraft.formatter.Style;
 import me.bounser.nascraft.market.MarketManager;
@@ -23,6 +24,8 @@ public class MarketEditorInvListener implements Listener {
 
 
     private final HashMap<Player, Integer> playerOffset = new HashMap<>();
+
+    private final HashMap<Player, Integer> playerHorizontalOffset = new HashMap<>();
 
     private static MarketEditorInvListener instance = null;
 
@@ -59,12 +62,53 @@ public class MarketEditorInvListener implements Listener {
             return;
         }
 
+        if (event.getRawSlot() == 48) {
+
+            new CategoryEditor(player);
+
+            return;
+        }
+
+
         if (event.getRawSlot() == 49) {
+
+            if (MarketManager.getInstance().getActive()) MarketManager.getInstance().stop();
+            else MarketManager.getInstance().resume();
+
+            ItemStack enabled;
+            ItemMeta metaEnabled;
+
+            if (MarketManager.getInstance().getActive()) {
+                enabled = new ItemStack(Material.LIME_DYE);
+
+                metaEnabled = enabled.getItemMeta();
+                metaEnabled.setDisplayName(ChatColor.GREEN + "§lMARKET ACTIVE");
+                metaEnabled.setLore(Arrays.asList(
+                        ChatColor.GRAY + "Click to stop the market.",
+                        ChatColor.GRAY + "Users won't be able to buy/sell."
+                ));
+
+            } else {
+                enabled = new ItemStack(Material.RED_DYE);
+
+                metaEnabled = enabled.getItemMeta();
+                metaEnabled.setDisplayName(ChatColor.RED + "§lMARKET STOPPED");
+                metaEnabled.setLore(Arrays.asList(
+                        ChatColor.GRAY + "Click to resume the market.",
+                        ChatColor.GRAY + "Users will be able to buy/sell."
+                ));
+            }
+
+            enabled.setItemMeta(metaEnabled);
+
+            event.getClickedInventory().setItem(49, enabled);
+        }
+
+        if (event.getRawSlot() == 50) {
 
             if (event.getCursor() != null && !event.getCursor().getType().equals(Material.AIR)) {
 
-                EditorManager.getInstance().startEditing(player, event.getCursor());
-
+                EditorManager.getInstance().startEditing(player, event.getCursor().clone());
 
             } else {
                 player.sendMessage(ChatColor.RED + "Drop an item to add it to the market!");
@@ -75,25 +119,63 @@ public class MarketEditorInvListener implements Listener {
 
         if (event.getRawSlot() == 0) {
 
-            if (playerOffset.get(player) == null) playerOffset.put(player, 3);
+            if (playerOffset.get(player) == null) initializeOffsets(player, 3, 0);
             else if (playerOffset.get(player) > 3)
                 playerOffset.put(player, playerOffset.get(player) - 1);
 
-            insertItems(event.getInventory(), playerOffset.get(player));
+            insertItems(event.getInventory(), playerOffset.get(player), playerHorizontalOffset.get(player));
 
             return;
         }
 
         if (event.getRawSlot() == 45) {
 
-            if (playerOffset.get(player) == null) playerOffset.put(player, 4);
+            if (playerOffset.get(player) == null) initializeOffsets(player, 4, 0);
             else if (playerOffset.get(player) < MarketManager.getInstance().getCategories().size()-1)
                 playerOffset.put(player, playerOffset.get(player) + 1);
 
-            insertItems(event.getInventory(), playerOffset.get(player));
+            insertItems(event.getInventory(), playerOffset.get(player), playerHorizontalOffset.get(player));
 
             return;
         }
+
+        if (event.getRawSlot() == 52) {
+
+            if (playerHorizontalOffset.get(player) == null) initializeOffsets(player, 3, 0);
+            else {
+                if (playerHorizontalOffset.get(player) > 0)
+                    playerHorizontalOffset.put(player, playerHorizontalOffset.get(player) - 1);
+            }
+
+            insertItems(event.getInventory(), playerOffset.get(player), playerHorizontalOffset.get(player));
+
+            return;
+        }
+
+        if (event.getRawSlot() == 53) {
+
+            if (playerHorizontalOffset.get(player) == null) initializeOffsets(player, 3, 1);
+            else {
+                int biggestCategory = 0;
+
+                for (Category category : MarketManager.getInstance().getCategories())
+                    if (category.getNumberOfItems() > biggestCategory) biggestCategory = category.getNumberOfItems();
+
+                if (playerHorizontalOffset.get(player) < biggestCategory-9)
+                    playerHorizontalOffset.put(player, playerHorizontalOffset.get(player) + 1);
+            }
+
+            insertItems(event.getInventory(), playerOffset.get(player), playerHorizontalOffset.get(player));
+
+            return;
+        }
+
+    }
+
+    public void initializeOffsets(Player player, int offset, int horizontalOffset) {
+
+        playerOffset.put(player, offset);
+        playerHorizontalOffset.put(player, horizontalOffset);
 
     }
 
@@ -108,7 +190,7 @@ public class MarketEditorInvListener implements Listener {
 
     }
 
-    public void insertItems(Inventory inventory, int offset) {
+    public void insertItems(Inventory inventory, int offset, int horizontalOffset) {
 
         List<Category> categories = new ArrayList<>();
 
@@ -119,7 +201,10 @@ public class MarketEditorInvListener implements Listener {
 
         for (Category category : categories) {
 
-            List<Item> items = new ArrayList<>(category.getItems());
+            List<Item> items = new ArrayList<>();
+
+            if (horizontalOffset < category.getNumberOfItems())
+                items = new ArrayList<>(category.getItems().subList(horizontalOffset, category.getNumberOfItems()));
 
             while (items.size() < 9)
                 items.add(null);
@@ -131,7 +216,7 @@ public class MarketEditorInvListener implements Listener {
                 if (item == null) {
                     inventory.clear((j+1)*9 + k);
                 } else {
-                    ItemStack itemStack = new ItemStack(item.getMaterial());
+                    ItemStack itemStack = item.getItemStack();
 
                     ItemMeta meta = itemStack.getItemMeta();
 
@@ -173,7 +258,7 @@ public class MarketEditorInvListener implements Listener {
             category = MarketManager.getInstance().getCategories().get(playerOffset.get(player)-3+i);
         }
 
-        return category.getItemOfIndex(slot - 9-(9*i));
+        return category.getItemOfIndex(slot - 9-(9*i) + (playerHorizontalOffset.get(player) == null ? 0 : playerHorizontalOffset.get(player)));
     }
 
 }
